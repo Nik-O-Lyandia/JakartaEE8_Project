ff2cb6b0d790ba3fbf4eb87c8b285eacded310df
Mykola Kovalenko <kovnikur@gmail.com>
2023-02-13 23:40:06 +0200

    external resource link added, link to front page added

--- src/main/java/MykolaPage.java
+++ src/main/java/MykolaPage.java
@@ -37,9 +37,14 @@ public class MykolaPage extends HttpServlet {
             out.println("<link rel=\"stylesheet\" href=\"WebContent/css/style.css\">");          
             out.println("</head>");
             out.println("<body>");
+            
             out.println("<h1>Servlet MykolaPage at '" + request.getRequestURL() + "'</h1>");
             out.println("<p>His profile image is follow: </p>");
             out.println("<img src=\"WebContent/imgs/Mykola_profile_img_2.jpg\" alt=\"profile img\" class=\"profile_img\"/>");
+            out.println("<p>His GitHub account: <a href=\"https://github.com/Nik-O-Lyandia\">GitHub</a></p>");
+            out.println("<br>");
+            out.println("<a href=\"/\">Back to Front Page</a>");
+            
             out.println("</body>");
             out.println("</html>");
         }
